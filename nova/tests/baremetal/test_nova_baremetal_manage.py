4257a9bf39fa524d0ac6fc1a292416c3ab67bf6c
Arata Notsu <notsu@virtualtech.jp>
2012-11-15 19:42:52 +0900

    Add tests for nova-baremetal-manage and some fixes
    Change-Id: I7eeab5c72bac2c622432e3447072cb193f02c3df

--- nova/tests/baremetal/test_nova_baremetal_manage.py
+++ nova/tests/baremetal/test_nova_baremetal_manage.py
@@ -20,7 +20,9 @@ import imp
 import os
 import sys
 
+from nova import context
 from nova import test
+from nova.virt.baremetal import db as bmdb
 
 
 TOPDIR = os.path.normpath(os.path.join(
@@ -45,3 +47,112 @@ class BareMetalDbCommandsTestCase(test.TestCase):
         self.commands.sync()
         v = self.commands.version()
         self.assertTrue(v > 0)
+
+
+class BareMetalNodeCommandsTestCase(test.TestCase):
+    def setUp(self):
+        super(BareMetalNodeCommandsTestCase, self).setUp()
+        self.flags(baremetal_sql_connection='sqlite:///:memory:')
+        self.commands = bm_man.BareMetalNodeCommands()
+        self.context = context.get_admin_context()
+        self.stubs.Set(context, "get_admin_context", lambda: self.context)
+        self.node = {"host": "host",
+                     "cpus": 8,
+                     "memory_mb": 8192,
+                     "local_gb": 128,
+                     "pm_address": "10.1.2.3",
+                     "pm_user": "pm_user",
+                     "pm_password": "pm_pass",
+                     "prov_mac_address": "12:34:56:78:90:ab",
+                     "prov_vlan_id": 1234,
+                     "terminal_port": 8000,
+                     }
+
+    def test_create(self):
+        values = {
+            'service_host': "host",
+            'cpus': 8,
+            'memory_mb': 8192,
+            'local_gb': 128,
+            'pm_address': "10.1.2.3",
+            'pm_user': "pm_user",
+            'pm_password': "pm_pass",
+            'prov_mac_address': "12:34:56:78:90:ab",
+            'prov_vlan_id': 1234,
+            'terminal_port': 8000,
+            'registration_status': 'done',
+            'task_state': None,
+        }
+        self.mox.StubOutWithMock(bmdb, "bm_node_create")
+        bmdb.bm_node_create(self.context, values).AndReturn({'id': 123})
+        self.mox.ReplayAll()
+        self.commands.create(**self.node)
+
+    def test_delete(self):
+        self.mox.StubOutWithMock(bmdb, "bm_node_destroy")
+        bmdb.bm_node_destroy(self.context, 12345)
+        self.mox.ReplayAll()
+        self.commands.delete(node_id=12345)
+
+
+class BareMetalInterfaceCommandsTestCase(test.TestCase):
+    def setUp(self):
+        super(BareMetalInterfaceCommandsTestCase, self).setUp()
+        self.flags(baremetal_sql_connection='sqlite:///:memory:')
+        self.commands = bm_man.BareMetalInterfaceCommands()
+        self.context = context.get_admin_context()
+        self.stubs.Set(context, "get_admin_context", lambda: self.context)
+
+    def test_create(self):
+        self.mox.StubOutWithMock(bmdb, "bm_node_get")
+        self.mox.StubOutWithMock(bmdb, "bm_interface_create")
+        bmdb.bm_node_get(self.context, 12345).\
+                AndReturn({'id': 12345,
+                           'prov_mac_address': '12:34:56:78:90:ab'})
+        bmdb.bm_interface_create(self.context,
+                                 bm_node_id=12345,
+                                 address="12:34:56:78:90:cd",
+                                 datapath_id="0xabc",
+                                 port_no="123").AndReturn(1)
+        self.mox.ReplayAll()
+        self.commands.create(12345,
+                             "12:34:56:78:90:cd",
+                             "abc",
+                             "123")
+
+    def test_delete(self):
+        self.mox.StubOutWithMock(bmdb, "bm_interface_destroy")
+        bmdb.bm_interface_destroy(self.context, 12345)
+        self.mox.ReplayAll()
+        self.commands.delete(if_id=12345)
+
+
+class BareMetalPxeIpCommandsTestCase(test.TestCase):
+    def setUp(self):
+        super(BareMetalPxeIpCommandsTestCase, self).setUp()
+        self.flags(baremetal_sql_connection='sqlite:///:memory:')
+        self.commands = bm_man.BareMetalPxeIpCommands()
+        self.context = context.get_admin_context()
+        self.stubs.Set(context, "get_admin_context", lambda: self.context)
+
+    def test_create(self):
+        self.mox.StubOutWithMock(bmdb, "bm_pxe_ip_create")
+        bmdb.bm_pxe_ip_create(self.context, "10.1.1.1", "10.1.1.2")
+        bmdb.bm_pxe_ip_create(self.context, "10.1.1.3", "10.1.1.4")
+        bmdb.bm_pxe_ip_create(self.context, "10.1.1.5", "10.1.1.6")
+        self.mox.ReplayAll()
+        self.commands.create(cidr_str="10.1.1.0/29")
+
+    def test_delete_by_id(self):
+        self.mox.StubOutWithMock(bmdb, "bm_pxe_ip_destroy")
+        bmdb.bm_pxe_ip_destroy(self.context, 12345)
+        self.mox.ReplayAll()
+        self.commands.delete(ip_id=12345)
+
+    def test_delete_by_cidr(self):
+        self.mox.StubOutWithMock(bmdb, "bm_pxe_ip_destroy_by_address")
+        bmdb.bm_pxe_ip_destroy_by_address(self.context, "10.1.1.1")
+        bmdb.bm_pxe_ip_destroy_by_address(self.context, "10.1.1.3")
+        bmdb.bm_pxe_ip_destroy_by_address(self.context, "10.1.1.5")
+        self.mox.ReplayAll()
+        self.commands.delete(cidr_str="10.1.1.0/29")
